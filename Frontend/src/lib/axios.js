5718a7574fecfaea04c1b0fb049a0e903fc18af4
codewithmanohar <mksahni2555@gmail.com>
2025-12-15 11:22:03 +0530

    commit : change vite_base_url

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "/api",
+    baseURL : process.env.VITE_BASE_URL,
 })
